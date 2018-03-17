fadffd8e93007f98863cba1567b855a85df7b64c
weixin0605 <weixin0605@163.com>
2018-03-17 13:01:51 +0800

    study

--- src/main/java/com/sws/hrmapp/controller/UserController.java
+++ src/main/java/com/sws/hrmapp/controller/UserController.java
@@ -6,7 +6,9 @@ import com.sws.hrmapp.util.common.HrmConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -19,16 +21,16 @@ public class UserController {
     @Qualifier("userService")
     private UserService userService;
 
-    @RequestMapping(value="/login")
-    public ModelAndView login(@RequestParam("loginname")String loginname, @RequestParam("password")String password, HttpSession session,ModelAndView mv){
-        User user = userService.login(loginname,password);
+    @RequestMapping(value="/login",method = RequestMethod.POST)
+    public String login(@RequestBody User userLogin, HttpSession session){
+        User user = userService.login(userLogin.getLoginname(),userLogin.getPassword());
         if(user!=null){
             session.setAttribute(HrmConstants.USER_SESSION,user);
-            mv.setViewName("redirect:/main");
+            return "/main";
         }else{
-            mv.addObject("message","登录名或密码错误，请重新输入");
-            mv.setViewName("forward:/loginForm");
+//            mv.addObject("message","登录名或密码错误，请重新输入");
+//            mv.setViewName("forward:/loginForm");
         }
-        return mv;
+        return "/loginForm";
     }
 }
--- src/main/java/com/sws/hrmapp/service/UserService.java
+++ src/main/java/com/sws/hrmapp/service/UserService.java
@@ -1,13 +1,53 @@
 package com.sws.hrmapp.service;
 
+
+import com.sws.hrmapp.mapper.UserMapper;
 import com.sws.hrmapp.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service("userService")
+//@Transactional(propagation= Propagation.REQUIRED,isolation = Isolation.DEFAULT)
+public class UserService{
 
-public interface UserService {
-    User login(String loginname, String password);
-    User findUserById(Integer id);
-//    List<User> findUser(User user, PageModel pageModel);
-    void removeUserById(Integer id);
-    void modifyUser(User user);
-    void addUser(User user);
+    @Autowired
+    private UserMapper userMapper;
+
+    @Transactional(readOnly = true)
+    public User login(String loginname, String password){
+        return userMapper.selectByLOginnameAndPassword(loginname,password);
+    }
+
+    @Transactional(readOnly = true)
+    public User findUserById(Integer id) {
+        return userMapper.selectByPrimaryKey(id);
+    }
+
+//    @Transactional(readOnly = true)
+//    public List<User> findUser(User user, PageModel pageModel) {
+//        Map<String,Object> params = new HashMap<String, Object>();
+//        params.put("user",user);
+//        int recordCount = userDao.count(params);
+//        pageModel.setRecordCount(recordCount);
+//        if(recordCount>0){
+//            params.put("pageModel",pageModel);
+//        }
+//        List<User> users = userDao.selectByPage(params);
+//        return users;
+//    }
+
+    public void removeUserById(Integer id) {
+        userMapper.deleteByPrimaryKey(id);
+    }
+
+    public void modifyUser(User user) {
+        userMapper.updateByPrimaryKeySelective(user);
+    }
+
+    public void addUser(User user) {
+        userMapper.insertSelective(user);
+    }
 }
