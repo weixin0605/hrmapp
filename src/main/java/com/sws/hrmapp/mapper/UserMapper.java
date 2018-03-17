17696ce9a4ffe06cc6b31088a3358f557cd1f106
weixin0605 <weixin0605@163.com>
2018-03-17 13:03:40 +0800

    study

---
+++ src/main/java/com/sws/hrmapp/mapper/UserMapper.java
@@ -0,0 +1,20 @@
+package com.sws.hrmapp.mapper;
+
+import com.sws.hrmapp.entity.User;
+import org.apache.ibatis.annotations.Param;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+
+    User selectByLOginnameAndPassword(@Param("loginname")String loginname, @Param("password") String password);
+}
